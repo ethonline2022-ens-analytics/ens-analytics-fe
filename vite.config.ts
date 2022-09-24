5bd6795533372e4fe80c159fcc9c4a161f7da7c1
Prawira Genestonlia <prawira.96@gmail.com>
2022-09-24 20:17:38 +0800

    feat: deploy.sh

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import Pages from 'vite-plugin-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), Pages(),]
+  plugins: [ react(), Pages() ],
+  base   : 'https://ethonline2022-ens-analytics.github.io/ens-analytics-fe/'
 })
