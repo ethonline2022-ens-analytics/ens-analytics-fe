f86c68d08fbe0bbb00b70091002ee91f89faaf8b
Prawira Genestonlia <prawira.96@gmail.com>
2022-09-24 21:20:44 +0800

    feat: 404 catch all

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,20 @@
-import React, { Suspense } from 'react'
-import { BrowserRouter as Router, useRoutes } from 'react-router-dom'
+import React, { Suspense, useEffect } from 'react'
+import { BrowserRouter as Router, useRoutes, useNavigate } from 'react-router-dom'
 
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import routes from '~react-pages'
 
 const App = () => {
+  const navigation = useNavigate()
+  useEffect(()=>{
+    const path = localStorage.getItem('path')
+    if (path) {
+      localStorage.removeItem('path')
+      navigation(path)
+    }
+  }, [])
+
   return <Suspense fallback={<p>Loading...</p>}>{useRoutes(routes)}</Suspense>
 }
 
