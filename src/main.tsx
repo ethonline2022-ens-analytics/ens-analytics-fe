d75eeceec65bb7bda8249d208230034ba887f79d
Prawira Genestonlia <prawira.96@gmail.com>
2022-09-24 20:33:59 +0800

    feat: tsconfig update

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,13 @@ import './index.css'
 import routes from '~react-pages'
 
 const App = () => {
-    return <Suspense fallback={<p>Loading...</p>}>{useRoutes(routes)}</Suspense>
+  return <Suspense fallback={<p>Loading...</p>}>{useRoutes(routes)}</Suspense>
 }
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-        <Router>
-            <App />
-        </Router>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
 )
